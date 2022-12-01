7365c90c2a641e7202aeb835866eb24ea4701063
Aitor Garcia Diez <bbminze@gmail.com>
2022-12-02 00:16:32 +0100

    First day

---
+++ AdventOfCode2022/Day01.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+namespace AdventOfCode2022;
+
+public static class Day01
+{
+    public static void Run()
+    {
+        // string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Data\Names.txt");
+        string lines = File.ReadAllText(@"01.txt").Replace("\r", "");
+        string[] allElves = lines.Split("\n\n");
+        int[] allCals = allElves
+            .Select(elf =>
+                elf.Split("\n")
+                    .Select(int.Parse)
+                    .Sum())
+            .OrderDescending().ToArray();
+        int f1 = allCals[0];
+        int f2 = allCals[1];
+        int f3 = allCals[2];
+        Console.WriteLine($"1st problem: {f1}");
+        Console.WriteLine($"2nd problem: {f1+f2+f3}");
+    }
+}
