c1671c43837edc383030ced46c44fa8338ec09c0
Aitor Garcia Diez <bbminze@gmail.com>
2022-12-06 15:32:11 +0100

    day6

---
+++ AdventOfCode2022/Day6.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode2022;
+
+public static class Day6
+{
+    public static void Run()
+    {
+        string input = Day.GetDayInput();
+
+        Console.WriteLine($"Part 1 Answer: {FindMessage(input, 4)}");
+        Console.WriteLine($"Part 2 Answer: {FindMessage(input, 14)}");
+    }
+
+    private static int FindMessage(string input, int length)
+    {
+        for (var i = 0; i < input.Length; i++)
+            if (input.Substring(i, length).ToCharArray().ToHashSet().Count == length)
+                return i + length;
+        return -1;
+    }
+}
